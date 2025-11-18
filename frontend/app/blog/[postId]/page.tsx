1f8d621f60c46b7fc71832bf05d15db0de8ead07
Kiran <kiran.sadara6730@gmail.com>
2025-11-18 20:28:44 +0530

    Dynamic routing with params for backend

--- frontend/app/blog/[postId]/page.tsx
+++ frontend/app/blog/[postId]/page.tsx
@@ -1,8 +1,17 @@
 // /blog/1 {this route will handle the dynamic routes}
 
-export default function BlogPage({params}:any){
+import axios from 'axios'
+import { useEffect } from 'react'
+export default async function BlogPage({params}:any){
 
     const postId = params.postId
+
+    useEffect(()=>{
+
+        const response = await axios.post(`http://localhost:3000/api/v1/get_data/${postId}`);
+        const data = response.data
+
+    },[])
     return (
         <div>
              Blog Page  : {postId}
