8673a99679de6154b53345eff523b51a5871e102
Kiran <kiran.sadara6730@gmail.com>
2025-11-18 20:30:36 +0530

    params is a promise

--- frontend/app/blog/[postId]/page.tsx
+++ frontend/app/blog/[postId]/page.tsx
@@ -1,17 +1,18 @@
 // /blog/1 {this route will handle the dynamic routes}
 
+"use client"
 import axios from 'axios'
 import { useEffect } from 'react'
 export default async function BlogPage({params}:any){
 
-    const postId = params.postId
+    const postId =  (await params).postId
 
-    useEffect(()=>{
+   
 
         const response = await axios.post(`http://localhost:3000/api/v1/get_data/${postId}`);
         const data = response.data
 
-    },[])
+    
     return (
         <div>
              Blog Page  : {postId}
